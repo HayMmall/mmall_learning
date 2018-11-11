499875148d6198ecfba3140450be700ccf50995e
caobabai <2942838129@qq.com>
2018-11-11 12:39:22 +0800

    lock

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -24,6 +24,7 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
+    @Scheduled(cron = "0 */1 * * * ?")
     public void closeOrderTaskV2() {
         log.info("关闭订单定时任务启动");
         long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
@@ -31,7 +32,7 @@ public class CloseOrderTask {
         Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
         if (setnxResult != null && setnxResult.intValue() == 1) {
             //如果返回值是1，代表設置成功，獲取鎖
-            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+//            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         } else {
             log.info("没有获得分布式锁:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         }
